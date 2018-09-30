6b1dc13e296b039a98e1e5341e50446f73cb4cf9
IsaacWarren <42949629+IsaacWarren@users.noreply.github.com>
2018-09-30 10:43:17 -0500

    Add files via upload

---
+++ parseGasStops.py
@@ -0,0 +1,50 @@
+import pandas
+import csv as cs
+
+#Import provided dataset
+csv = pandas.read_csv("/home/isaac/Downloads/Copy of fuel_card_exportcsvalphabet.csv")
+
+#Create a dataset with one entry for each station
+namededuped = csv.drop_duplicates("Merchant Street Address")
+
+#Declare variables
+out = []
+i = 0
+offset = 0
+
+#Add address and merchant name to return list (out)
+for index, row in namededuped.iterrows():
+    addr = row["Merchant Street Address"]
+    name = row["Merchant Name"]
+    city = row["Merchant City"]
+    out.append([name, str(addr) + " " + str(city) + " Illinois", 0, 0, 0, 0, 0, 0])
+
+
+#Calculate the running totals of transactions, money spent, and difference from average
+for each in range(len(out) - 2):
+    addr = out[each][1]
+    for inner, row in csv.iterrows():
+        inneraddr = csv.loc[offset]["Merchant Street Address"]
+        i = offset
+
+        while i <= 6306 and addr.find(csv.loc[i+1]["Merchant Street Address"]) != -1:
+            print ([addr, csv.loc[i+1]["Merchant Street Address"]])
+            out[each][6] += ((csv.loc[i]["Unit Cost"]) - 2.35)
+            out[each][7] += 1
+            out[each][5] += (csv.loc[i]["Unit Cost"])
+            i += 1
+            offset = i
+#Calculate the average cost, difference from mean, and % difference from mean using the data collected above
+for each in out:
+
+    if each[7] != 0:
+        each[2] = each[5]/each[7]
+        each[3] = each[6]/each[7]
+    each[4] = ((2.35 - each[2]) / 2.35) * 100.0
+
+#Write the final out list to a csv
+with open('/home/isaac/Documents/export.csv', 'w', newline='') as myfile:
+    wr = cs.writer(myfile, quoting=cs.QUOTE_ALL)
+    for each in out:
+        wr.writerow(each)
+
